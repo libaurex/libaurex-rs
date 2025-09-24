d3b4b64a1e2d81153fbf647325e2dcf48f502e8f
t800-x <t800.x.official@gmail.com>
2025-09-24 19:46:21 +0530

    Add async decoding

--- src/engine.rs
+++ src/engine.rs
@@ -1,5 +1,5 @@
-use std::{ffi::c_void, io::{self, Read}, ptr, sync::mpsc::{self, Receiver, Sender}, thread::{self, Thread}, time::Instant};
-use ffmpeg_next::decoder::decoder;
+use std::{ffi::c_void, io::{self, Read}, ptr, sync::{mpsc::{self, Receiver, Sender}, Arc, Mutex}, thread::{self, Thread}, time::Instant};
+use ffmpeg_next::{decoder::decoder, packet::Mut};
 use soxr::{format, Soxr};
 use crate::ffi::data_callback;
 use miniaudio_aurex::{self as miniaudio, ma_device_config_init, ma_device_start, ma_device_stop};
@@ -8,6 +8,10 @@ use miniaudio_aurex::{self as miniaudio, ma_device_config_init, ma_device_start,
 use ffmpeg_next::{
     self as av, ffi::AVAudioFifo, frame::Audio as AudioFrame, media, sys
 };
+
+pub struct AudioFifo(pub *mut AVAudioFifo);
+
+unsafe impl Send for AudioFifo {}
 
 pub enum PlayerState {
     LOADING = 0,
@@ -23,15 +27,15 @@ enum CMD {
 
 pub struct AudioEngine {
     device: miniaudio::ma_device,
-    buffer: *mut AVAudioFifo,
+    buffer: Arc<Mutex<AudioFifo>>,
     channels: i32,
-    sample_rate: i32,
-    decoder: Option<av::codec::decoder::audio::Audio>,
+    sample_rate: Arc<Mutex<i32>>,
+    decoder: Arc<Mutex<Option<av::codec::decoder::audio::Audio>>>,
     state: PlayerState,
-    format_ctx: Option<av::format::context::Input>,
+    format_ctx: Arc<Mutex<Option<av::format::context::Input>>>,
     device_config: miniaudio::ma_device_config,
     initialised: bool,
-    tx: Option<Sender<CMD>>,
+    tx: Option<Sender<CMD>>
 }
 
 impl AudioEngine {
@@ -39,9 +43,9 @@ impl AudioEngine {
         let mut device: miniaudio::ma_device = unsafe {
             std::mem::zeroed()
         };
-        let buffer: *mut AVAudioFifo = unsafe {
-            sys::av_audio_fifo_alloc(sys::AVSampleFormat::AV_SAMPLE_FMT_S32, 2, 1)
-        }; 
+        let buffer_ptr= unsafe {sys::av_audio_fifo_alloc(sys::AVSampleFormat::AV_SAMPLE_FMT_S32, 2, 1)};
+        let buffer = Arc::new(Mutex::new(AudioFifo(buffer_ptr)));
+
         let mut device_config = unsafe {
             ma_device_config_init(miniaudio::ma_device_type_ma_device_type_playback)
         };
@@ -50,7 +54,7 @@ impl AudioEngine {
         device_config.playback.channels = 0;
         device_config.sampleRate = 0;
         device_config.dataCallback = Some(data_callback);
-        device_config.pUserData = buffer as *mut c_void;
+        device_config.pUserData = buffer.lock().unwrap().0 as *mut c_void;
 
         if cfg!(target_os = "windows") {
             println!("Detected Windows");
@@ -72,10 +76,10 @@ impl AudioEngine {
                 device: device, 
                 buffer: buffer,
                 channels: device.playback.channels as i32,
-                sample_rate: device.sampleRate as i32,
-                decoder: None,
+                sample_rate: Arc::new(Mutex::new(device.sampleRate as i32)),
+                decoder: Arc::new(Mutex::new(None)),
                 state: PlayerState::EMPTY,
-                format_ctx: None,
+                format_ctx: Arc::new(Mutex::new(None)),
                 device_config: device_config,
                 initialised: false,
                 tx: None
@@ -106,90 +110,26 @@ impl AudioEngine {
             .nth(audio_stream_index)
             .expect("Stream Disappeared").parameters();
         let codec_ctx = av::codec::context::Context::from_parameters(codec_params).expect("Failed to allocate codec context");
-        self.decoder = Some(codec_ctx.decoder()
-            .audio().expect("Failed to open decoder."));
+        // self.decoder = Arc::new(Mutex::new(
+        //     Some(codec_ctx.decoder()
+        //     .audio().expect("Failed to open decoder."))
+        // ));
+        let dec = codec_ctx.decoder().audio().expect("Failed to open decoder.");
+        * self.decoder.lock().unwrap() = Some(dec);
 
-        let decoder = self.decoder.as_mut().expect("Decoder not Initialised");
-        self.format_ctx = Some(format_ctx);
+        let decoder = self.decoder.lock().unwrap().as_mut().expect("Decoder not Initialised");
+        // self.format_ctx = Arc::new(Mutex::new(Some(format_ctx)));
+        * self.format_ctx.lock().unwrap() = Some(format_ctx);
         self.state = PlayerState::LOADED;
         println!("Loaded {}", &file);
+
+        _ = self.tx.as_mut().unwrap().send(CMD::Start);
 
         Ok(())
     }
 
     pub fn play(&mut self) -> Result<(), i32> {
-        let decoder = self.decoder.as_mut().expect("Decoder not initialized");
-
-        let mut resampler = av::software::resampling::Context::get(
-            decoder.format(), 
-            decoder.channel_layout(), 
-            decoder.rate(), 
-            av::format::Sample::I32(av::format::sample::Type::Packed), //Basically just changing sample format as soxr is only capable of resampling the rate 
-            decoder.channel_layout(), 
-            decoder.rate()
-        ).expect("Failed to init resampler");
-
-        let mut soxr_resampler = Soxr::<format::Interleaved<i32, 2>>::new(decoder.rate() as f64, self.sample_rate as f64).expect("Failed to setup Soxr");
-
-        let format_ctx = self.format_ctx.as_mut().expect("Format Context not initialised");
-
-        let audio_stream_index = format_ctx.streams()
-            .best(media::Type::Audio)
-            .expect("No audio stream found.").index();
-
-        for (stream, packet) in format_ctx.packets() {
-            if stream.index() != audio_stream_index {
-                continue;
-            }
-
-            decoder.send_packet(&packet).expect("Failed to send packet to decoder.");
-            let mut frame = AudioFrame::empty();
-
-            while decoder.receive_frame(&mut frame).is_ok() {
-                let mut resampled_frame = AudioFrame::empty();
-                _ = resampler.run(&frame, &mut resampled_frame);
-
-                let input_samples: &[[i32; 2]] = bytemuck::cast_slice(resampled_frame.data(0));
-                let mut output_buf = vec![[0i32; 2]; (input_samples.len() as usize * self.sample_rate as usize) / decoder.rate() as usize];
-
-                let res = soxr_resampler.process(input_samples, &mut output_buf).unwrap();
-
-                let mut soxr_frame = AudioFrame::new(
-                    av::format::Sample::I32(av::format::sample::Type::Packed),
-                    res.output_frames,
-                    av::ChannelLayout::STEREO
-                );
-                soxr_frame.set_rate(self.sample_rate as u32);
-
-                // Copy the processed samples into the FFmpeg frame
-                let data_plane = soxr_frame.data_mut(0);
-                let dst_slice: &mut [[i32; 2]] = bytemuck::cast_slice_mut(data_plane);
-                dst_slice[..res.output_frames].copy_from_slice(&output_buf[..res.output_frames]);
-
-                unsafe {
-                    // 1) Get a mutable pointer to the first byte of the frame buffer:
-                    let data_ptr0 = soxr_frame.data_mut(0).as_mut_ptr() as *mut c_void;
-
-                    // 2) Build a small array of channel-pointers:
-                    //Now normally only one pointer would mean mono audio but we are using interleaved pcm so all channels are mashed into one.
-                    let mut data_ptrs: [*mut c_void; 1] = [data_ptr0];
-
-                    let written = sys::av_audio_fifo_write(
-                        self.buffer,
-                        data_ptrs.as_mut_ptr(),
-                        soxr_frame.samples() as i32,
-                    );
-
-                    if written < 0 {
-                        // Todo
-                    }
-                }
-            }
-        }
-
-        // self.reinit_device();
-
-
+        
         if unsafe { miniaudio::ma_device_start(&mut self.device) } != miniaudio::ma_result_MA_SUCCESS {
             println!("Failed to start device");
         }
@@ -201,18 +141,92 @@ impl AudioEngine {
         unsafe {
             std::mem::drop(self.device);
             self.device = std::mem::zeroed();
-            self.device_config.pUserData = self.buffer as *mut c_void;
+            self.device_config.pUserData = Arc::into_raw(self.buffer.clone()) as *mut c_void;
             miniaudio::ma_device_init(ptr::null_mut(), &self.device_config, &mut self.device);
         }
         Ok(())
     }
 
     fn spawn_decoder_thread(&mut self, rx: Receiver<CMD>) -> Result<(), i32> {
+        let decoder_handle = self.decoder.clone();
+        let sample_rate_handle = self.sample_rate.clone();
+        let format_ctx_handle = self.format_ctx.clone();
+        let buffer_handle = self.buffer.clone();
+
         thread::spawn(move || {
             for cmd in rx {
                 match cmd {
                     CMD::Start => {
-                        
+                        let mut decoder_binding = decoder_handle.lock().unwrap();
+                        let decoder = decoder_binding.as_mut().unwrap();
+
+                            let mut resampler = av::software::resampling::Context::get(
+                                decoder.format(), 
+                                decoder.channel_layout(), 
+                                decoder.rate(), 
+                                av::format::Sample::I32(av::format::sample::Type::Packed), //Basically just changing sample format as soxr is only capable of resampling the rate 
+                                decoder.channel_layout(), 
+                                decoder.rate()
+                            ).expect("Failed to init resampler");
+
+                            let mut soxr_resampler = Soxr::<format::Interleaved<i32, 2>>::new(decoder.rate() as f64, *sample_rate_handle.lock().unwrap() as f64).expect("Failed to setup Soxr");
+
+                            let mut format_ctx_binding = format_ctx_handle.lock().unwrap();
+                            let format_ctx = format_ctx_binding.as_mut().expect("Format Context not initialised");
+
+                            let audio_stream_index = format_ctx.streams()
+                                .best(media::Type::Audio)
+                                .expect("No audio stream found.").index();
+
+                            for (stream, packet) in format_ctx.packets() {
+                                if stream.index() != audio_stream_index {
+                                    continue;
+                                }
+
+                                decoder.send_packet(&packet).expect("Failed to send packet to decoder.");
+                                let mut frame = AudioFrame::empty();
+
+                                while decoder.receive_frame(&mut frame).is_ok() {
+                                    let mut resampled_frame = AudioFrame::empty();
+                                    _ = resampler.run(&frame, &mut resampled_frame);
+
+                                    let input_samples: &[[i32; 2]] = bytemuck::cast_slice(resampled_frame.data(0));
+                                    let mut output_buf = vec![[0i32; 2]; (input_samples.len() as usize * *sample_rate_handle.lock().unwrap() as usize) / decoder.rate() as usize];
+
+                                    let res = soxr_resampler.process(input_samples, &mut output_buf).unwrap();
+
+                                    let mut soxr_frame = AudioFrame::new(
+                                        av::format::Sample::I32(av::format::sample::Type::Packed),
+                                        res.output_frames,
+                                        av::ChannelLayout::STEREO
+                                    );
+                                    soxr_frame.set_rate(*sample_rate_handle.lock().unwrap() as u32);
+
+                                    // Copy the processed samples into the FFmpeg frame
+                                    let data_plane = soxr_frame.data_mut(0);
+                                    let dst_slice: &mut [[i32; 2]] = bytemuck::cast_slice_mut(data_plane);
+                                    dst_slice[..res.output_frames].copy_from_slice(&output_buf[..res.output_frames]);
+
+                                    unsafe {
+                                        // 1) Get a mutable pointer to the first byte of the frame buffer:
+                                        let data_ptr0 = soxr_frame.data_mut(0).as_mut_ptr() as *mut c_void;
+
+                                        // 2) Build a small array of channel-pointers:
+                                        //Now normally only one pointer would mean mono audio but we are using interleaved pcm so all channels are mashed into one.
+                                        let mut data_ptrs: [*mut c_void; 1] = [data_ptr0];
+
+                                        let written = sys::av_audio_fifo_write(
+                                            buffer_handle.lock().unwrap().0,
+                                            data_ptrs.as_mut_ptr(),
+                                            soxr_frame.samples() as i32,
+                                        );
+
+                                        if written < 0 {
+                                            // Todo
+                                        }
+                                    }
+                                }
+                            }
                     }
                 }
             }            
--- src/ffi.rs
+++ src/ffi.rs
@@ -1,38 +1,69 @@
 use ffmpeg_next::{ffi::AVAudioFifo, sys};
-use std::{ffi::c_void, ptr};
+use std::{ffi::c_void, ptr, sync::Arc};
 use miniaudio_aurex as miniaudio;
+use crate::engine::AudioFifo;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn data_callback(
-    p_device: *mut miniaudio::ma_device, 
-    p_output: *mut c_void, 
-    _p_input: *const c_void, 
-    frame_count: miniaudio::ma_uint32
+    p_device: *mut miniaudio::ma_device,
+    p_output: *mut c_void,
+    _p_input: *const c_void,
+    frame_count: miniaudio::ma_uint32,
 ) {
     unsafe {
-        let fifo = (*p_device).pUserData as *mut AVAudioFifo;
-        
-        // Create a single-element array of pointers
-        let mut data_ptrs = [p_output as *mut c_void];
-        let data_ptrs_ptr = data_ptrs.as_mut_ptr() as *mut *mut c_void;
+        // reconstruct Arc without changing refcount permanently:
+        let arc_ptr = (*p_device).pUserData as *const std::sync::Mutex<AudioFifo>;
+        let arc = Arc::from_raw(arc_ptr);         // temporarily owns one ref
+        // clone to bump refcount for our short usage, then restore original raw
+        let arc_clone = arc.clone();
+        let _ = Arc::into_raw(arc);               // restore raw so original refcount unaffected
 
-        let got = sys::av_audio_fifo_read(fifo, data_ptrs_ptr, frame_count as i32);
-        if got < frame_count as i32 {
-            // Handle underrun by zero-filling remaining buffer
-            let bytes_per_sample = sys::av_get_bytes_per_sample(
-                sys::AVSampleFormat::AV_SAMPLE_FMT_S16
-            ) as usize;
-            let channels = (*p_device).playback.channels as usize;
-            let bytes_per_frame = bytes_per_sample * channels;
-            let written_bytes = got as usize * bytes_per_frame;
-            let total_bytes = frame_count as usize * bytes_per_frame;
-            let remaining_bytes = total_bytes - written_bytes;
-            
-            ptr::write_bytes(
-                p_output.add(written_bytes) as *mut u8,
-                0,
-                remaining_bytes
-            );
+        // non-blocking lock: if busy, consider underrun
+        match arc_clone.try_lock() {
+            Ok(mut guard) => {
+                let fifo = guard.0;
+                let available = sys::av_audio_fifo_size(fifo);
+                let mut data_ptrs = [p_output as *mut c_void];
+                let got = if available >= frame_count as i32 {
+                    sys::av_audio_fifo_read(fifo, data_ptrs.as_mut_ptr(), frame_count as i32)
+                } else {
+                    // read whatever is available (may be 0)
+                    if available > 0 {
+                        sys::av_audio_fifo_read(fifo, data_ptrs.as_mut_ptr(), available)
+                    } else {
+                        0
+                    }
+                };
+
+                if got < frame_count as i32 {
+                    // zero-fill the rest
+                    let bytes_per_sample = sys::av_get_bytes_per_sample(
+                        sys::AVSampleFormat::AV_SAMPLE_FMT_S16
+                    ) as usize;
+                    let channels = (*p_device).playback.channels as usize;
+                    let bytes_per_frame = bytes_per_sample * channels;
+                    let written_bytes = got as usize * bytes_per_frame;
+                    let total_bytes = frame_count as usize * bytes_per_frame;
+                    let remaining_bytes = total_bytes.saturating_sub(written_bytes);
+
+                    ptr::write_bytes(
+                        (p_output as *mut u8).add(written_bytes),
+                        0,
+                        remaining_bytes,
+                    );
+                }
+            }
+            Err(_) => {
+                // lock busy: consider it's an underrun — zero-fill whole buffer
+                let bytes_per_sample = sys::av_get_bytes_per_sample(
+                    sys::AVSampleFormat::AV_SAMPLE_FMT_S16
+                ) as usize;
+                let channels = (*p_device).playback.channels as usize;
+                let bytes_per_frame = bytes_per_sample * channels;
+                let total_bytes = frame_count as usize * bytes_per_frame;
+                ptr::write_bytes(p_output as *mut u8, 0, total_bytes);
+            }
         }
+        // arc_clone drops here, decreasing refcount as expected
     }
 }
